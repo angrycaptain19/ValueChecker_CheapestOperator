b97175e33a079cddc6545b390280c3916261c601
shardkul <53390662+shardkul@users.noreply.github.com>
2021-03-01 05:03:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 19:45:19 2021
+
+@author: Shardul Kulkarni
+"""
+
+import os
+import pandas as pd
+import numpy as np
+import re
+
+
+# data_path = os.getcwd() + '\\Data\\'
+
+
+class cheapest_operator_utility:
+    
+#########################################################################################            
+# Function Name - generate_data
+# Description   - Generate 'n' lists of operator with random prefixes and rates ()
+# Parameters    - 
+#       n - Number of operator Lists to generate
+# 
+#########################################################################################
+
+    def generate_data(n):
+        data_path = os.getcwd() + '\\Data\\'
+        try:
+            os.mkdir(data_path)
+        except:
+            pass
+            
+        for opr in range(n):
+            df = pd.DataFrame()
+            np.random.seed(40+opr)
+            df['prefix'] = pd.Series(np.random.randint(100,3000,5000))
+            df['rate'] = pd.Series(np.random.uniform(0.1,10.0,5000)).round(2)
+            df.prefix = df.prefix.apply(lambda x : str(x))
+            df.to_csv(data_path + 'operator_'+str(opr+1)+'.csv',index=False)
+        
+            
+#########################################################################################            
+# Function Name - pick_longest_matching_prefix
+# Description   - From passed dataframe and number, the function calculates longest matching prefix
+# Parameters    -  
+#       file_df - Dataframe containing prefix and rate
+#       numbr   - Inputted Phone no.  
+#########################################################################################
+    
+    def pick_longest_matching_prefix(file_df,numbr):
+        
+        file_df['prefix'] = file_df['prefix'].apply(lambda x : re.sub('\D', '', str(x)))
+        file_df['flag'] = file_df['prefix'].apply(lambda x : np.where(re.search('^'+str(x), str(numbr)) == None , 0 ,1 ))
+        file_df['len'] = file_df['prefix'].apply(lambda x : len(str(x)))
+        file_df = file_df[(file_df['flag'] == 1)].sort_values(by=['len','rate'],ascending = [False,True]).head(1)
+        return file_df[['prefix','rate','operator']]
+    
+    
+#########################################################################################            
+# Function Name - load_file
+# Description   - Loads file from specified path
+# Parameters    -  
+#     file_name - Name of the file to be loaded
+#     file_path - Path from where the file needs to be loaded
+#########################################################################################
+
+    def load_file(file_name,file_path):
+        
+        operator_name = os.path.splitext(file_name)[0]
+        
+        # Check if file is valid csv
+        try:
+            file_df = pd.read_csv(file_path+file_name)
+            file_df.dropna(inplace = True)
+        except:
+            print('Skipping file : ',file_name,', as it is not a csv')
+            return None , 0 
+        
+        file_df = file_df[[i for i in list(file_df.columns) if i in ['prefix','rate']]]
+
+        # Csv is not as per the format
+        if  ('prefix' not in list(file_df.columns)) | ('rate' not in list(file_df.columns)):
+            print(file_name,'is not as per the format')
+            return None , 0
+        
+        file_df['operator'] = operator_name
+        
+        return file_df , 1
+   
+    
+#########################################################################################            
+# Function Name - find_cheapest_operator
+# Description   - Loads all the lists of operators from specified path and returns the 
+#                 cheapest operator
+# Parameters    -  
+#       numbr   - Inputted Phone no.
+#       path    - Path from where the files need to be loaded
+#########################################################################################
+
+    def find_cheapest_operator(numbr,path):
+        
+        if (re.search('[a-zA-Z]', str(numbr)) != None) | (re.search('[^a-zA-Z0-9]+', str(numbr)) != None) | (len(re.sub('\D', '', str(numbr))) != 12):
+            return 'Invalid Phone Number'
+        
+        # Extract numbers from input numbr
+        numbr = re.sub('\D', '', str(numbr))
+        
+        # Check the validty of the path
+        try:
+            dir_list = os.listdir(path)
+        except:
+            return 'Invalid Path'
+        
+        # Check if their are files in specified path
+        if dir_list == []:
+            return 'No files found'
+        
+        error_files = []
+        tmp = pd.DataFrame()
+        
+        for file in dir_list:
+                    
+            opr_df, error_code = cheapest_operator_utility.load_file(file, path)
+            
+            if error_code != 1 :
+                error_files.append(file)
+                continue
+                
+            opr_df = cheapest_operator_utility.pick_longest_matching_prefix(opr_df, numbr)
+            tmp = tmp.append(opr_df)
+                
+        # Check if all files are invalid
+        if len(error_files) == len(dir_list):
+            return 'All files are invalid'
+        
+        # Return 
+        tmp = tmp.sort_values('rate',ascending=True).head(1)['operator'].reset_index(drop=True)
+        if tmp.shape[0] == 0:
+            return 'No operators available'
+        else:
+            return tmp[0]
+  
+# cheapest_operator_utility.generate_data(10)
+
+
+
---
+++ test.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 19:02:05 2021
+
+@author: Shardul Kulkarni
+"""
+import os
+from main import *
+import unittest
+
+
+class test_cheapest_operator_utility(unittest.TestCase):
+    
+    def test_1(self):
+        path = os.getcwd() +'\\Data\\' 
+        self.assertEqual(cheapest_operator_utility.find_cheapest_operator('222923654234',path), 'operator_15')  
+
+    def test_2(self):
+        path = os.getcwd() +'\\Dat\\' 
+        self.assertEqual(cheapest_operator_utility.find_cheapest_operator('222923654234',path), 'Invalid Path')  
+        
+    def test_3(self):
+        path = os.getcwd() +'\\Data\\' 
+        self.assertEqual(cheapest_operator_utility.find_cheapest_operator('+46727602584',path), 'Invalid Phone Number')
+
+    def test_4(self):
+        path = os.getcwd() +'\\Data\\' 
+        self.assertEqual(cheapest_operator_utility.find_cheapest_operator('46727602584as',path), 'Invalid Phone Number')
+        
+    def test_5(self):
+        path = os.getcwd() +'\\Data\\' 
+        self.assertEqual(cheapest_operator_utility.find_cheapest_operator('123456789',path), 'Invalid Phone Number')
+        
+    def test_6(self):
+        path = os.getcwd() +'\\Data 1\\'
+        self.assertEqual(cheapest_operator_utility.find_cheapest_operator('222923654234',path), 'No files found')
+           
+    def test_7(self):
+        path = os.getcwd() +'\\Data 2\\' 
+        self.assertEqual(cheapest_operator_utility.find_cheapest_operator('222923654234',path), 'All files are invalid')
+        
+    def test_8(self):
+        path = os.getcwd() +'\\Data\\' 
+        self.assertEqual(cheapest_operator_utility.find_cheapest_operator('000234324890',path), 'No operators available')
+    
+
+if __name__ == '__main__':
+    unittest.main()
